ac92af5bfa9fb18c7a2308039e6b470000a1be93
zhangruiqian <3480197195@qq.com>
2024-02-27 18:19:50 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,118 @@
+
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import {onMounted, ref} from "vue";
+import {msgApi} from "@/common/network";
+import {message} from "ant-design-vue";
+import logo from '@/assets/logo.jpg'
+
+const msg = ref('')
+const chats = ref<{ msg: string, isQ: boolean }[]>([])
+
+const onAskClick = () => {
+    if (msg.value == '') {
+        message.warning('请输入问题！');
+        return
+    }
+    chats.value.push({
+        msg: msg.value,
+        isQ: true
+    })
+    getMsg()
+}
+
+const getMsg = async () => {
+    const res = await msgApi(msg.value)
+    console.log(res.data)
+    chats.value.push({
+        msg: res.data,
+        isQ: false
+    })
+}
+
+onMounted(() => {
+})
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="chats">
+        <div class="chat" v-for="(item, index) in chats" :key="index">
+            <div class="question" v-if="item.isQ">
+                {{ item.msg }}
+            </div>
+            <div class="answer" v-else>
+                <div class="header">
+                    <img class="face" :src="logo" alt=""/>
+                    猫娘小糖
+                </div>
+                {{ item.msg }}
+            </div>
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <el-affix position="bottom" :offset="0">
+        <div class="message-box">
+            <el-input
+                v-model="msg"
+                :autosize="{ minRows: 2, maxRows: 4 }"
+                type="textarea"
+                placeholder="请输入提问信息"
+            />
+            <el-button type="primary" round class="button" @click="onAskClick">提问</el-button>
+        </div>
+    </el-affix>
+    <!--    <a-affix offset-bottom="0">-->
+    <!--        <a-flex class="message-box" :vertical="false">-->
+    <!--            <a-textarea-->
+    <!--                    class="input"-->
+    <!--                    allow-clear-->
+    <!--                    v-model:value="msg"-->
+    <!--                    placeholder="请输入提问信息"/>-->
+    <!--            <a-button class="button" @click="onAskClick">提问</a-button>-->
+    <!--        </a-flex>-->
+    <!--    </a-affix>-->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+.chats {
+    padding-bottom: 100px;
+
+    .chat {
+        padding: 10px;
+
+        .question {
+            padding: 20px;
+            border-radius: 30px;
+            border: #e7edf1 solid 2px;
+        }
+
+        .answer {
+            padding: 20px;
+            border-radius: 30px;
+            background-color: #e7edf1;
+
+            .header {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                gap: 10px;
+                margin-bottom: 10px;
+                color: #505050;
+
+                .face {
+                    border-radius: 40px;
+                    width: 50px;
+                    height: 50px;
+                }
+            }
+        }
+    }
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.message-box {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    padding: 20px;
+    gap: 10px;
 }
 </style>
